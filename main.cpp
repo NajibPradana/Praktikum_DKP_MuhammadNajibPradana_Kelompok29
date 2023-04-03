8a41140f8180268dd1bb8e05170736456919f730
NajibPradana <mnpradana13@gmail.com>
2023-04-03 18:46:15 +0700

    update 030423_1846

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
+class Sistem_Parkiran{
+public :
+    int jam_masuk,jam_keluar,sel_jam;
+    int biaya,tambahan,biaya_total;
+    int jenis;
+
+void jenis_kendaraan(){
+cout << "Masukan Jenis Kendaraan Anda : \n1. Mobil\n2. Motor"<<endl;
+cout << "Pilihan Anda : " ;
+cin >> jenis;
+}
+
+void tarif_perkendaraan(){
+switch (jenis){
+		case 1  :
+			biaya=3000;
+			tambahan=1000;
+			cout << "Biaya Parkir Mobil          = Rp 3000(2 jam pertama)\n             Biaya Tambahan = Rp 1000/jam" << endl;
+			break;
+		case 2  :
+			biaya=2000;
+			tambahan=500;
+			cout << "Biaya Parkir Motor            = Rp 2000(2 jam pertama)\n             Biaya Tambahan   = Rp 500/jam" << endl ;
+			break;
+		default :
+			biaya=0;
+	}
+}
 int main()
 {
     cout << "Hello world!" << endl;
